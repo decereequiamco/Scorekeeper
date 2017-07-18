fcc9a99a51267e616610a239ca0d7f0a88b6733e
Deceree Quiamco <decereequiamco@gmail.com>
2017-07-18 23:42:22 +0800

    (4) Implement score counters.

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -1,15 +1,100 @@
 package tech.hyperdev.scorekeeper.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.view.*;
 
 import tech.hyperdev.scorekeeper.R;
+import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context;
+    TextView textView2;
+    ImageButton btnPlus;
+    ImageButton btnMinus;
+    TextView tvTeamName;
+    FragmentManager team1;
+    FragmentManager team2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView2 = (TextView)findViewById(R.id.textView2);
+        btnPlus = (ImageButton)findViewById(R.id.btnPlus);
+        btnMinus = (ImageButton) findViewById(R.id.btnMinus);
+        tvTeamName = (TextView)findViewById(R.id.tvTeamName);
+
+        context=this;
+
+//            team1= getSupportFragmentManager();
+//
+//            team1.beginTransaction()
+//                 .replace(R.id.team1,ScoreFragment.newInstance(Integer.parseInt(("Team 1")),0))
+//                    .addToBackStack("team1")
+//                    .commit();
+//
+//
+//        team2= getSupportFragmentManager();
+//
+//        team2.beginTransaction()
+//                .replace(R.id.team2,ScoreFragment.newInstance(Integer.parseInt("Team 2"),0))
+//                .addToBackStack("team2")
+//                .commit();
+
+
+
+
+        btnPlus.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                String present_value_string = textView2.getText().toString();
+                int present_value_int = Integer.parseInt(present_value_string);
+                present_value_int++;
+
+                textView2.setText(String.valueOf(present_value_int));
+            }
+        });
+
+        btnMinus.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v)
+            {
+                String present_value_string = textView2.getText().toString();
+                int present_value_int = Integer.parseInt(present_value_string);
+                present_value_int--;
+                if(present_value_int<=0)
+                {
+                    btnMinus.setEnabled(false);
+                }
+                else{
+                    btnMinus.setEnabled(true);
+
+                }
+
+                textView2.setText(String.valueOf(present_value_int));
+            }
+        });
+
     }
+
+
+
+
+
+    public void changeText(View num) {
+        ScoreFragment scoreFragment = (ScoreFragment) getSupportFragmentManager().findFragmentById(R.id.team1);
+        scoreFragment.plusOnclick(num);
+
+
+  }
 }
--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -5,7 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
 
@@ -14,8 +15,35 @@ import tech.hyperdev.scorekeeper.R;
  */
 public class ScoreFragment extends Fragment {
 
-    private static Button btnPlus;
-    private static Button btnMinus;
+    private static ImageButton btnPlus;
+    private static ImageButton btnMinus;
+    private static TextView textView2;
+    private static TextView tvTeamName;
+    private static int team1;
+    private static int team2;
+
+
+    int a = 0;
+
+
+
+//    public static ScoreFragment newInstance(int team1, int team2) {
+//        Bundle bundle = new Bundle();
+//        bundle.putInt("teamName", team1);
+//        bundle.putInt("teamNum",team2);
+//
+//        ScoreFragment fragment = new ScoreFragment();
+//        fragment.setArguments(bundle);
+//
+//        return fragment;
+//    }
+//    private void readBundle(Bundle bundle) {
+//        if (bundle != null) {
+//            team1 = Integer.parseInt(bundle.getString("name"));
+//            team2 = Integer.parseInt(bundle.getString("name2"));
+//        }
+//    }
+
 
     public ScoreFragment() {
         // Required empty public constructor
@@ -26,9 +54,36 @@ public class ScoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_score, container, false);
+        View view = inflater.inflate(R.layout.fragment_score, container, false);
+
+        btnPlus = (ImageButton) view.findViewById(R.id.btnPlus);
+        btnMinus = (ImageButton) view.findViewById(R.id.btnMinus);
+        textView2 = (TextView) view.findViewById(R.id.textView2);
+        tvTeamName = (TextView) view.findViewById(R.id.tvTeamName);
+
+//        readBundle(getArguments());
+//        tvTeamName.setText(String.format(String.valueOf(team1+team2)));
+
+
+        btnPlus.setOnClickListener(
+                new View.OnClickListener(){
+                    public void onClick(View v){
+                        plusOnclick(v);
+                    }
+                }
+        );
+
+        return view;
+    }
+    public void plusOnclick(View v){
+
+
+
+        textView2.setText(""+a);
 
 
     }
+
+
 
 }
